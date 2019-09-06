72503b17e251b86125174ddbf7fbfb1d2e49cc7f
Jonas Horvei <jonashorvei1@gmail.com>
2019-09-06 06:00:36 +0200

    Added error message hwen more than 10 countries are displayed in filter.

--- countries/src/components/Countries.js
+++ countries/src/components/Countries.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Country from './Country'
 
 const Countries = ({countries}) => {
-
+    
     if(countries.length === 1) {
         return(
             <Country key={countries[0].name}
                      country={countries[0]} />
         )
+    } else if(countries.length > 10) {
+        return <div>Too many matches, specify another filter.</div>
     } else {
         return(
              countries.map(country => 
